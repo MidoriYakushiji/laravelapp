37cc7833778f717ec542d89f18bffad291ee88eb
MidoriYakushiji <m.yakushiji1008@gmail.com>
2022-06-16 12:31:56 +0900

    P55 & jissyu2

--- routes/web.php
+++ routes/web.php
@@ -13,34 +13,27 @@
 
 //use Illuminate\Routing\Route;
 
-$html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size:16pt; color:#999; }
-h1 { font-size:100pt; text-align:right; color:#eee;
-   margin:-40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-   <h1>Hello</h1>
-   <p>This is sample page.</p>
-   <p>これは、サンプルで作ったページです。</p>
-</body>
-</html>
-EOF;
+// $html = <<<EOF
+// <html>
+// <head>
+// <title>Hello</title>
+// <style>
+// body {font-size:16pt; color:#999; }
+// h1 { font-size:100pt; text-align:right; color:#eee;
+//    margin:-40px 0px -50px 0px; }
+// </style>
+// </head>
+// <body>
+//    <h1>Hello</h1>
+//    <p>This is sample page.</p>
+//    <p>これは、サンプルで作ったページです。</p>
+// </body>
+// </html>
+// EOF;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('hello/{id?}/{pass?}', 'HelloController@index');
-
-// Route::get('hello',function () use ($html) {
-//     return $html;
-//  });
-
+Route::get('hello','HelloController@index');
+Route::get('hello/other','HelloController@other');
 
 /* Route::get('hello/{msg?}',function ($msg='no message.') {
 
@@ -64,3 +57,7 @@ Route::get('hello/{id?}/{pass?}', 'HelloController@index');
     
        return $html;
     }); */
+
+
+//------実習用------
+Route::get('jissyu2','JissyuController@index');
