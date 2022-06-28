9fd549122182df33d3de343fec7260d731ce679c
MidoriYakushiji <m.yakushiji1008@gmail.com>
2022-06-28 14:44:56 +0900

    P160

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
-
+use Validator;
 class HelloController extends Controller
 {
     public function index(Request $request)
