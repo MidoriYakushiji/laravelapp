af26a69446533a361ad90869a9ac364fbb0e9ad1
MidoriYakushiji <m.yakushiji1008@gmail.com>
2022-07-01 13:40:40 +0900

    P194

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -27,6 +27,25 @@ class HelloController extends Controller
     {
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
+
+//------データの挿入------
+    public function add(Request $request)
+    {
+        return view('hello.add');
+    }
  
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
+        return redirect('/hello');
+    }
+  
 }  
+
+
 
